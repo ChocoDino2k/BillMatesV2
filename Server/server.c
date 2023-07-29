e9bfcd7b9a7b7752b546ecde812fd04f9b2568b9
Logan Cover <coverlog555@gmail.com>
2023-07-29 15:28:52 -0400

    Automatic commit 20230729-15:28:52

--- Server/server.c
+++ Server/server.c
@@ -131,8 +131,8 @@ int main() {
 
 			//check if read all headers
 			int i = totalBytesRead; 
-			printf("total num of bytes: %d\n", i);
-			printf("%s\n", buffer);
+			//printf("total num of bytes: %d\n", i);
+			//printf("%s\n", buffer);
 			while ( !readAllHeaders & (i > 3) ) {
 				if (
 				buffer[i - 4] == '\r' && buffer[i - 3] == '\n' &&
@@ -182,6 +182,7 @@ int main() {
 int charUntil(char * string, char character) {
 	unsigned int i = 0;
 	while ( *(string + i) != character) {
+		printf("%c", *(string + i) );
 		i++;
 	}
 	return i;
